cd73376d02ada26c21a824d21bea09584aa27f05
Vry0z <arthur.lonfils@gmail.com>
2024-09-23 10:51:17 +0200

    end ex1.2

--- exercices/ex1.2/app.ts
+++ exercices/ex1.2/app.ts
@@ -6,6 +6,16 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+
+let counter = 0;
+app.use((_req, _res, next) => {
+    
+    if(_req.method == "GET"){
+      console.log("GET counter : " + ++counter);
+    }
+    next();
+});
+
 app.use("/films", movieRouter);
 
 export default app;
